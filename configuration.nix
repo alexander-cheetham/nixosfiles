cae90353073228e99d712d0cfa16026fe35a593b
Alex-PC <alexcool12390@gmail.com>
2024-06-14 15:39:51 +0100

    semicolon

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,7 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  nix.settings.experimental-features = ["nix-command" "flakes"]
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
